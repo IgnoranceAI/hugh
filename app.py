161185d20c187d0be635decf30c97401b576f7f8
Charlie Guo <charlierguo@gmail.com>
2023-03-07 06:56:06 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,149 @@
+import openai
+import os
+import requests
+import uuid
+from flask import Flask, request, jsonify, send_file, render_template
+
+
+# Add your OpenAI API key
+OPENAI_API_KEY = ""
+openai.api_key = OPENAI_API_KEY
+
+# Add your ElevenLabs API key
+ELEVENLABS_API_KEY = ""
+ELEVENLABS_VOICE_STABILITY = 0.30
+ELEVENLABS_VOICE_SIMILARITY = 0.75
+
+# Choose your favorite ElevenLabs voice
+ELEVENLABS_VOICE_NAME = "Hugh"
+ELEVENLABS_ALL_VOICES = []
+
+app = Flask(__name__)
+
+
+def get_voices() -> list:
+    """Fetch the list of available ElevenLabs voices.
+
+    :returns: A list of voice JSON dictionaries.
+    :rtype: list
+
+    """
+    url = "https://api.elevenlabs.io/v1/voices"
+    headers = {
+        "xi-api-key": ELEVENLABS_API_KEY
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()["voices"]
+
+
+def transcribe_audio(filename: str) -> str:
+    """Transcribe audio to text.
+
+    :param filename: The path to an audio file.
+    :returns: The transcribed text of the file.
+    :rtype: str
+
+    """
+    with open(filename, "rb") as audio_file:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript.text
+
+
+def generate_reply(text: str) -> str:
+    """Generate a ChatGPT response.
+
+    :param text: The user prompt.
+    :returns: The ChatGPT response.
+    :rtype: str
+
+    """
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": text},
+        ]
+    )
+    return response["choices"][0]["message"]["content"]
+
+
+def generate_audio(text: str, output_path: str = "") -> str:
+    """Converts
+
+    :param text: The text to convert to audio.
+    :type text : str
+    :param output_path: The location to save the finished mp3 file.
+    :type output_path: str
+    :returns: The output path for the successfully saved file.
+    :rtype: str
+
+    """
+    voices = ELEVENLABS_ALL_VOICES
+    try:
+        voice_id = next(filter(lambda v: v["name"] == ELEVENLABS_VOICE_NAME, voices))["voice_id"]
+    except StopIteration:
+        voice_id = voices[0]["voice_id"]
+    url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}"
+    headers = {
+        "xi-api-key": ELEVENLABS_API_KEY,
+        "content-type": "application/json"
+    }
+    data = {
+        "text": text,
+        "voice_settings": {
+            "stability": ELEVENLABS_VOICE_STABILITY,
+            "similarity_boost": ELEVENLABS_VOICE_SIMILARITY,
+        }
+    }
+    response = requests.post(url, json=data, headers=headers)
+    with open(output_path, "wb") as output:
+        output.write(response.content)
+    return output_path
+
+
+@app.route('/')
+def index():
+    """Render the index page."""
+    return render_template('index.html', voice=ELEVENLABS_VOICE_NAME)
+
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    """Transcribe the given audio to text using Whisper."""
+    if 'file' not in request.files:
+        return 'No file found', 400
+    file = request.files['file']
+    recording_file = f"{uuid.uuid4()}.wav"
+    recording_path = f"uploads/{recording_file}"
+    os.makedirs(os.path.dirname(recording_path), exist_ok=True)
+    file.save(recording_path)
+    transcription = transcribe_audio(recording_path)
+    return jsonify({'text': transcription})
+
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    """Generate a ChatGPT response from the given text, then convert it to audio using ElevenLabs."""
+    text = request.get_json(force=True).get("text", "")
+    reply = generate_reply(text)
+    reply_file = f"{uuid.uuid4()}.mp3"
+    reply_path = f"outputs/{reply_file}"
+    os.makedirs(os.path.dirname(reply_path), exist_ok=True)
+    generate_audio(reply, output_path=reply_path)
+    return jsonify({'text': reply, 'audio': f"/listen/{reply_file}"})
+
+
+@app.route('/listen/<filename>')
+def listen(filename):
+    """Return the audio file located at the given filename."""
+    return send_file(f"outputs/{filename}", mimetype="audio/mp3", as_attachment=False)
+
+
+if __name__ == '__main__':
+    app.run()
+
+if ELEVENLABS_API_KEY:
+    if not ELEVENLABS_ALL_VOICES:
+        ELEVENLABS_ALL_VOICES = get_voices()
+    if not ELEVENLABS_VOICE_NAME:
+        ELEVENLABS_VOICE_NAME = ELEVENLABS_ALL_VOICES[0]["name"]
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Ask Hugh
+Ask Hugh is a web application that allows you to record audio, transcribe it, and then ask a question based on the transcription. The application uses the Whisper API, the ChatGPT API, and the ElevenLabs API. 
+
+### Installation
+1. Clone this repository:
+`git clone https://github.com/IgnoranceAI/hugh.git`
+
+2. Install the Python packages:
+`pip install -r requirements.txt`
+
+3. Add your API keys to `app.py`:
+`OPENAI_API_KEY = "YOUR API KEY HERE"`
+`ELEVENLABS_API_KEY = "YOUR API KEY HERE"`
+
+### Usage
+1. Start the Flask server:
+`python app.py`
+2. Open your web browser and navigate to http://localhost:5000.
+3. Click the “Record” button to start recording audio. Speak into your microphone and then click the button again when you’re done. The transcription will appear in the text box.
+4. Alternatively, type your question into the text box and click the “Ask” button to submit your question and generate a response.
+5. The response will appear in the area below the audio player. It will begin playing and typing once it is ready.
+
+
+### Credits
+Record and Ask was created by Artificial Ignorance using Flask, OpenAI’s Whisper, OpenAI’s ChatGPT, and ElevenLabs. 
+
+### License
+This project is licensed under the MIT License - see the LICENSE file for details.
